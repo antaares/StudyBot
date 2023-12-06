5de79da90dd8595e1cc7ae95b5b92dae65ac83fd
antaares <jrismoilov@gmail.com>
2023-12-07 01:05:49 +0500

    kkkkk

--- core/handlers/users/for_user.py
+++ core/handlers/users/for_user.py
@@ -20,7 +20,6 @@ async def getId(message: types.Message, state: FSMContext):
         await message.answer_chat_action(action="upload_document")
         with open(path, 'rb') as file:
             await message.answer_document(document=file, caption=f"{id} id raqamli natijalar fayli.")
-            await message.answer("Javoblar varaqasi ID raqamini kiriting.")
             await state.finish()
             return
     await message.answer("Siz yuborgan id ga mos fayl topilmadi. ID raqam to‘g‘ri ekanligini tekshirib ko'ring!")
